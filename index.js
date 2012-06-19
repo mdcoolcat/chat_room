d5cf697f14e0fdcce7a63ed018a5f18e190b35a9
Danielle <danielle.mei1989@gmail.com>
2012-06-18 20:49:07 -0400

    info sys format. uuid error!

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 			    return v.toString(16);
 			}), color = uuid.slice(-3);
 	var message_bubble_tpl = '<div style="display:inline-block;max-width:460px;background-color:#{background};color:#fff;text-shadow: #000 0 1px 1px;font-size: 15px;background-image:-moz-linear-gradient(rgba(255,255,255,0.8)0%,rgba(0,0,0,0)100%);background-image:-webkit-gradient(linear,left top,left bottom,from(rgba(255,255,255,0.8)),to(rgba(0,0,0,0)));border:0;margin:10px 0px 10px 0px;padding:6px 40px 6px 20px;border-radius:50px;-moz-border-radius:50px;-webkit-border-radius:50px;overflow:hidden;-o-transition:all 0.3s;-moz-transition:all 0.3s;-webkit-transition:all 0.3s;transition:all 0.3s;position:relative;"><strong>{username}&nbsp;-&nbsp;</strong>&nbsp;{message}</div></br>';
+	var sysinfo_tpl = '<div style="display:inline-block;max-width:460px;background-color:black;color:#fff;font-size:12px;text-shadow: #000 0 1px 1px;font-size: 15px;background-image:-moz-linear-gradient(rgba(255,255,255,0.8)0%,rgba(0,0,0,0)100%);background-image:-webkit-gradient(linear,left top,left bottom,from(rgba(255,255,255,0.8)),to(rgba(0,0,0,0)));border:0;margin:5px 0px 5px 0px;padding:3px 20px 3px 10px;border-radius:20px;-moz-border-radius:50px;-webkit-border-radius:50px;overflow:hidden;-o-transition:all 0.3s;-moz-transition:all 0.3s;-webkit-transition:all 0.3s;transition:all 0.3s;position:relative;">{time}&nbsp;-&nbsp;<strong>{username}</strong>&nbsp;{message}</div></br>';
 	username.focus();
 	
 	//username input
@@ -39,7 +40,6 @@
 			name_change.innerHTML = 'Hide Name Input';
 		} else {
 			$('#username').parent().slideUp();	
-			input.focus();
 			name_change.innerHTML = 'Change Nickname';
 		}
 	});
@@ -75,17 +75,17 @@
 				if (!ids[uuid]) {
 					ids[uuid] = true;
 					my_name = name;
-					
-					//if (name_change.style.display == 'none') {	//new user? later use uuid...
 					name_change.style.display = 'block';
-					list.innerHTML = 'Welcome: ' + name + '</br>' + list.innerHTML;
-					//} else
-						//list.innerHTML = old + ' changed nickname: ' +name + '</br>' + list.innerHTML;
+					var sysinfo = new_sysinfo(name, 'joins the chat', format_time());
+					list.innerHTML = sysinfo.innerHTML + list.innerHTML;
+					//list.innerHTML = 'Welcome: ' + name + format_time() + '</br>' + list.innerHTML;
 				} else {
 					if (name != my_name) {
 						var old = my_name;
 						my_name = name;
-						list.innerHTML = old + ' changed nickname: ' +name + '</br>' + list.innerHTML;
+						var sysinfo = new_sysinfo(old, 'changed nickname: '+ name, format_time());
+						list.innerHTML = sysinfo.innerHTML + list.innerHTML;
+						//list.innerHTML = old + ' changed nickname: ' +name + format_time() + '</br>' + list.innerHTML;
 					}
 				}
 				username.value = '';
@@ -113,6 +113,33 @@
 				});
 		return bubble;
 	}
+	
+	function new_sysinfo(name, info, time) {
+		var bubble = document.createElement('div');
+		// Update The Message Text Body
+		bubble.innerHTML = PUBNUB.supplant(sysinfo_tpl,
+				{
+					'username' : name
+							|| 'Anonymous',
+					'message' : info,
+					'time' : time
+				});
+		return bubble;
+	}
+	
+	//time format
+	function format_time() {
+		var dt = new Date();
+	    var hours = dt.getHours();
+	    var minutes = dt.getMinutes();
+	    // the above dt.get...() functions return a single digit
+	    // so I prepend the zero here when needed
+	    if (hours < 10) 
+	    	hours = '0' + hours;
+	    if (minutes < 10) 
+	    	minutes = '0' + minutes;
+	    return hours + ":" + minutes;
+	} 
 	
 	//input area resize
 	$('#input').focus(function() {
