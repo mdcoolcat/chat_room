b582858c474d5aa43bcf5edfaa320f9c196bd4d9
Danielle <danielle.mei1989@gmail.com>
2012-06-21 22:49:33 -0400

    count chars,but visibility not fixed yet

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 (function() {
-	//var users = new Array();
-	//var my_name = "Anonymous";
-	var msgbox = PUBNUB.$('box'), input = PUBNUB.$('input'), channel = 'chat', username = PUBNUB.$('username'), list = PUBNUB.$('info-list'), name_change = PUBNUB.$('nickame-change'),
-			ids = {}, publishes = 1, br_rx = /[\r\n<>]/g, space_rx = /^\s+|\s+$/g, 
-			max_name = 16, max_msg = 100,
-			uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-			    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-			    return v.toString(16);
-			}), color = uuid.slice(-3);
+	var channel = 'chat',
+		msgbox = PUBNUB.$('box'), list = PUBNUB.$('info-list'), input = PUBNUB.$('input'),  username = PUBNUB.$('username'), 
+		name_change = PUBNUB.$('nickame-change'), counter = PUBNUB.$('count'), counter_div = PUBNUB.$('count-div'),
+		ids = {}, publishes = 1, br_rx = /[\r\n<>]/g, space_rx = /^\s+|\s+$/g, 
+		max_name = 20, max_msg = 140,
+		uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+		    return v.toString(16);
+		}), color = uuid.slice(-3), 
+		sys_type = { USER: 0, MENTIONED: 1 },
+		sys_color = ['000000', '0099ff']; 
 	var message_bubble_tpl = '<div style="display:inline-block;max-width:460px;background-color:#{background};color:#fff;text-shadow: #000 0 1px 1px;font-size: 15px;background-image:-moz-linear-gradient(rgba(255,255,255,0.8)0%,rgba(0,0,0,0)100%);background-image:-webkit-gradient(linear,left top,left bottom,from(rgba(255,255,255,0.8)),to(rgba(0,0,0,0)));border:0;margin:10px 0px 10px 0px;padding:6px 40px 6px 20px;border-radius:50px;-moz-border-radius:50px;-webkit-border-radius:50px;overflow:hidden;-o-transition:all 0.3s;-moz-transition:all 0.3s;-webkit-transition:all 0.3s;transition:all 0.3s;position:relative;"><strong>{username}&nbsp;-&nbsp;</strong>&nbsp;{message}</div></br>';
-	var sysinfo_tpl = '<div style="display:inline-block;max-width:460px;background-color:black;color:#fff;font-size:12px;text-shadow: #000 0 1px 1px;font-size: 15px;background-image:-moz-linear-gradient(rgba(255,255,255,0.8)0%,rgba(0,0,0,0)100%);background-image:-webkit-gradient(linear,left top,left bottom,from(rgba(255,255,255,0.8)),to(rgba(0,0,0,0)));border:0;margin:5px 0px 5px 0px;padding:3px 20px 3px 10px;border-radius:20px;-moz-border-radius:50px;-webkit-border-radius:50px;overflow:hidden;-o-transition:all 0.3s;-moz-transition:all 0.3s;-webkit-transition:all 0.3s;transition:all 0.3s;position:relative;">{time}&nbsp;-&nbsp;<strong>{username}</strong>&nbsp;{message}</div></br>';
+	var sysinfo_tpl = '<div style="display:inline-block;max-width:460px;background-color:#{background};color:#fff;font-size:12px;text-shadow: #000 0 1px 1px;font-size: 15px;background-image:-moz-linear-gradient(rgba(255,255,255,0.8)0%,rgba(0,0,0,0)100%);background-image:-webkit-gradient(linear,left top,left bottom,from(rgba(255,255,255,0.8)),to(rgba(0,0,0,0)));border:0;margin:5px 0px 5px 0px;padding:3px 20px 3px 10px;border-radius:20px;-moz-border-radius:50px;-webkit-border-radius:50px;overflow:hidden;-o-transition:all 0.3s;-moz-transition:all 0.3s;-webkit-transition:all 0.3s;transition:all 0.3s;position:relative;">{time}&nbsp;-&nbsp;<strong>{username}</strong>&nbsp;{message}</div></br>';
+	
+	//initialized
 	username.focus();
-	console.log(uuid);
-	console.log('initial', ids[uuid]);
+	counter.innerHTML = max_msg;
+	//counter_div.style.display = 'none';
 	//username input
 	PUBNUB.bind('keydown', username, function(e) {
 		if (e.keyCode == 13) {
@@ -37,16 +41,23 @@
 	//nickname change
 	PUBNUB.bind('mousedown,touchstart', name_change, function() {
 		if (name_change.innerHTML == 'Change Nickname') {
-			$('#username').parent().slideDown();	
+			$('#username').parent().slideDown(80);	
 			username.focus();
 			name_change.innerHTML = 'Hide Name Input';
 		} else {
-			$('#username').parent().slideUp();	
+			$('#username').parent().slideUp(80);	
 			name_change.innerHTML = 'Change Nickname';
 		}
 	});
 	
 	//message input
+	PUBNUB.bind('keyup', input, function(e) {	//count chars
+		if (e.keyCode != 13) {
+			counter.innerHTML = max_msg - input.value.length;
+		}
+		return true;
+	});
+		
 	PUBNUB.bind('keydown', input, function(e) {
 		if (e.keyCode == 13) {
 			if (input.value.replace(space_rx, "").length == 0) {
@@ -54,7 +65,6 @@
 				input.value = '';
 				return false;
 			}
-			console.log('in publish msg:', ids[uuid]);
 			PUBNUB.publish({
 				'channel' : channel,
 				'message' : {
@@ -65,7 +75,8 @@
 					'content' : input.value.slice(0, max_msg).replace(br_rx, '')
 				}
 			});
-		}
+			counter.innerHTML = max_msg;
+		} 
 		return true;
 	});
 	
@@ -75,21 +86,19 @@
 			if (message['type'] == 'user') {
 				var name = message['content'];
 				var the_uuid = message['uuid'];
-				//if (users.indexOf(name) < 0) {	//new user or change name
-					//users.push(name);
 				console.log(the_uuid);
 				console.log('id', ids[the_uuid]);
 				if (!ids[the_uuid]) {
 					ids[the_uuid] = name;
 					name_change.style.display = 'block';
-					var sysinfo = new_sysinfo(name, 'joins the chat', format_time());
+					var sysinfo = new_sysinfo(sys_type.USER, name, 'joins the chat', format_time());
 					list.innerHTML = sysinfo.innerHTML + list.innerHTML;
 					//list.innerHTML = 'Welcome: ' + name + format_time() + '</br>' + list.innerHTML;
 				} else {
 					if (name != ids[the_uuid]) {
 						var old = ids[the_uuid];
 						ids[the_uuid] = name;
-						var sysinfo = new_sysinfo(old, 'changed nickname: '+ name, format_time());
+						var sysinfo = new_sysinfo(sys_type.USER, old, 'changed nickname: '+ name, format_time());
 						list.innerHTML = sysinfo.innerHTML + list.innerHTML;
 						//list.innerHTML = old + ' changed nickname: ' +name + format_time() + '</br>' + list.innerHTML;
 					}
@@ -121,12 +130,13 @@
 		return bubble;
 	}
 	
-	function new_sysinfo(name, info, time) {
+	function new_sysinfo(sys_type, name, info, time) {
 		var bubble = document.createElement('div');
 		// Update The Message Text Body
 		bubble.innerHTML = PUBNUB.supplant(sysinfo_tpl,
 				{
 					'username' : name || 'Anonymous',
+					'background' : sys_color[sys_type],
 					'message' : info,
 					'time' : time
 				});
@@ -149,17 +159,32 @@
 	
 	//input area resize
 	$('#input').focus(function() {
-		$(this).animate(
-				{height: "90px"}, 700
-		);
+		if (input.style.height != '100px') {
+			console.log('changing display...', counter_div.style.display);
+			if (counter_div.style.display == 'none')
+				counter_div.style.display = 'block';
+			console.log('changed display...', counter_div.style.display);
+			
+			$(this).animate(
+					{height: "100px"}, 80
+			);
+		}	//else, already resize
+			
 	});
 	$('#input').blur(function() {
 		console.log(this.value.replace(space_rx, "").length);
-		if (this.value.replace(space_rx, "").length == 0) {
-			$(this).animate(
-					{height: "45px"}, 700
-			);
-			this.value = "";
+		if (input.style.height != '45px') {
+			if (this.value.replace(space_rx, "").length == 0) {
+				if (counter_div.style.display == 'block')
+					counter_div.style.display = 'none';
+				
+				$(this).animate(
+						{height: "45px"}, 80
+				);
+				this.value = "";
+				//document.getElementById('count-div').style.display = 'none';
+				//var x = document.getElementById('count-div');
+			}
 		}
 	});
 	
